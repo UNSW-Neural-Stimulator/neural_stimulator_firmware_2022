f6000c30d23abc43875f7b8ea07ec39842ee60ea
Jasper Di Francesco <jasper.difrancesco@mail.com>
2022-04-08 13:43:47 +1000

    Add switches example

--- other/Swtiches/Switches.cydsn/main_cm4.c
+++ other/Swtiches/Switches.cydsn/main_cm4.c
@@ -42,20 +42,33 @@
 #include "project.h"
 #include "ctdac/cy_ctdac.h"
 
+#define STIM_ENABLE P9_0_PORT
+#define DUMMY_LOAD  P9_4_PORT
+#define SHORT_ELECTRODE P9_1_PORT
+#define TOGGLE_OUTPUT P9_3_PORT
+
+#define STIM_ENABLE_NUM P9_0_NUM
+#define DUMMY_LOAD_NUM  P9_4_NUM
+#define SHORT_ELECTRODE_NUM P9_1_NUM
+#define TOGGLE_OUTPUT_NUM P9_3_NUM
+
 // Note there is kind of not a maximum for each of these phases, it is limited only by 
 // uint overflow (each phase cannot be more than 4294s)
-#define INTERPULSE_GAP 100
-#define CATHODIC_PHASE 50
-#define INTERPHASE_GAP 50
-#define ANODIC_PHASE 50
+#define INTERPULSE_GAP 100u
+#define CATHODIC_PHASE 50u
+#define INTERPHASE_GAP 50u
+#define ANODIC_PHASE 50u
 
 // Stage indicates where we are in a pulse, 0 is interpulse, 1 is cathodic, 2 is interphase
 // 3 is anodic
-uint32_t stage = 0u;
+uint32_t phase = 0u;
 // Counter indicates how far through a phase we are 
 uint32_t counter = 0u;
 // Current code is what the voltage is going to be set as 
 uint32_t currentCode = 0u;
+
+uint32_t vdac_values[4] = {2048u, 4095u - 25, 2048u, 0u + 25};
+uint32_t phase_timings[4] = {INTERPULSE_GAP, CATHODIC_PHASE, INTERPHASE_GAP, ANODIC_PHASE};
 
 
 void userIsr(void);
@@ -83,13 +96,20 @@ int main(void)
     __enable_irq();
 
     /* Configure the interrupt and provide the ISR address. */
-    (void)Cy_SysInt_Init(&SysInt_1_cfg, userIsr);
+    (void)Cy_SysInt_Init(&SysInt_cfg, userIsr);
 
     /* Enable the interrupt. */
-    NVIC_EnableIRQ(SysInt_1_cfg.intrSrc);
+    NVIC_EnableIRQ(SysInt_cfg.intrSrc);
+    
+    /* Set default pin values */
+    Cy_GPIO_Write(STIM_ENABLE, STIM_ENABLE_NUM, 1);//set stim enable to high 
+    Cy_GPIO_Write(DUMMY_LOAD, DUMMY_LOAD_NUM, 0);//set dummy load to low
+    Cy_GPIO_Write(SHORT_ELECTRODE, SHORT_ELECTRODE_NUM, 1);//set short electrode to high 
+    Cy_GPIO_Write(TOGGLE_OUTPUT, TOGGLE_OUTPUT_NUM, 0);//set toggle output to low
+   
 
     /* Start the component. */
-    VDAC_1_Start();
+    VDAC_Start();
     
     for(;;)
     {
@@ -116,44 +136,23 @@ void userIsr(void)
     uint8_t intrStatus;
 
     /* Check that an interrupt occurred in the VDAC component instance. */
-    intrStatus = VDAC_1_GetInterruptStatus();
+    intrStatus = VDAC_GetInterruptStatus();
     if (intrStatus)
     {
         /* Clear the interrupt. */
-        VDAC_1_ClearInterrupt();
-        
-        /* Set the next value that the DAC will output. */
-        VDAC_1_SetValueBuffered(currentCode);
-
+        VDAC_ClearInterrupt();
+       
         counter++;
-        // Interpulse
-        if (stage == 0u && counter >= (INTERPULSE_GAP / 2)) 
-        {
-            currentCode = 0u;
-            stage = 1u;
+        if (counter >= (phase_timings[phase] / 2)) {
+            phase++;
+            phase %= 4; 
+            currentCode = vdac_values[phase];
             counter = 0u;
-        } 
-        // Cathodic
-        else if (stage == 1u && counter >= (CATHODIC_PHASE / 2)) 
-        {
-            currentCode = 1950u;
-            stage = 2u;
-            counter = 0u;
-        } 
-        // Interphase
-        else if (stage == 2u && counter >= (INTERPHASE_GAP / 2))
-        {
-            currentCode = 3900u;
-            stage = 3u;
-            counter = 0u;
+            Cy_GPIO_Write(SHORT_ELECTRODE, SHORT_ELECTRODE_NUM, (phase == 0) ? 1 : 0);
+            Cy_GPIO_Write(TOGGLE_OUTPUT, TOGGLE_OUTPUT_NUM, (phase & 1) ? 1 : 0);
         }
-        // Anodic
-        else if (stage == 3u && counter >= (ANODIC_PHASE / 2)) 
-        {
-            currentCode = 1950u;
-            stage = 0u;
-            counter = 0u;
-        }
+        
+        VDAC_SetValueBuffered(currentCode);
     }
 }
 
