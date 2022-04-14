b4de6a0807f075d7417d845fe88383f932f4330d
Jasper Di Francesco <jasper.difrancesco@mail.com>
2022-04-14 15:31:47 +1000

    Add bluetooth functionality to switches

--- other/Swtiches/Switches.cydsn/main_cm0p.c
+++ other/Swtiches/Switches.cydsn/main_cm0p.c
@@ -14,13 +14,14 @@
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
+    Cy_BLE_Start(0);
     /* Enable CM4.  CY_CORTEX_M4_APPL_ADDR must be updated if CM4 memory layout is changed. */
     Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR);
 
     for(;;)
     {
         /* Put Cortex-M0+ in DeepSleep mode */
-        Cy_SysPm_DeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
+        Cy_BLE_ProcessEvents();
     }
 }
 
