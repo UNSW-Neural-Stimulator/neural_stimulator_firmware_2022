99f999f17d582221af2d37f026b58a78899a87d0
Jasper Di Francesco <jasper.difrancesco@mail.com>
2022-07-06 15:38:45 +1000

    Improve error messages

--- firmware/BLE_DAC/BLE_DAC/BLE_read.cydsn/main_cm4.c
+++ firmware/BLE_DAC/BLE_DAC/BLE_read.cydsn/main_cm4.c
@@ -18,59 +18,66 @@
 
 SemaphoreHandle_t bleSemaphore;
 
-uint32_t val1 = 1;
-uint32_t val2 = 2;
+uint8_t vals[2] = {0};
 
 // BLE event handler
 void genericEventHandler(uint32_t event, void *eventParameter) {
     cy_stc_ble_gatts_write_cmd_req_param_t *writeReqParameter;
-    printf("BLE Event handler, event 0x%x\n", event);
+    printf("\nBLE Event handler, event 0x%x\n", event);
     // Take an action based on current event
     switch (event) {
         case CY_BLE_EVT_STACK_ON:
         
         case CY_BLE_EVT_GAP_DEVICE_DISCONNECTED:
             printf("disconnected\r\n");
-            Cy_GPIO_Write(P6_3_PORT, P6_3_PIN, 1);                           // RED is ON
-            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, 0);                           // GREEN is OFF
+            Cy_GPIO_Write(P6_3_PORT, P6_3_PIN, 0);                           // RED is ON
+            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, 1);                           // GREEN is OFF
             Cy_BLE_GAPP_StartAdvertisement(CY_BLE_ADVERTISING_FAST, CY_BLE_PERIPHERAL_CONFIGURATION_0_INDEX);
             break;
         case CY_BLE_EVT_GATT_CONNECT_IND:
             printf("connected\r\n");
-            Cy_GPIO_Write(P6_3_PORT, P6_3_PIN, 0);                           // RED is OFF
-            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, 1);                           // GREEN is ON
+            Cy_GPIO_Write(P6_3_PORT, P6_3_PIN, 1);                           // RED is OFF
+            Cy_GPIO_Write(P7_1_PORT, P7_1_PIN, 0);                           // GREEN is ON
             break;
             
         case CY_BLE_EVT_GATTS_WRITE_REQ:
-            printf("Write req\n");
+            printf("Write req: ");
             writeReqParameter = (cy_stc_ble_gatts_write_cmd_req_param_t *) eventParameter;
             uint8_t command[5];
             for (int i = 0; i < writeReqParameter->handleValPair.value.len; i++) {
                 command[i] = writeReqParameter->handleValPair.value.val[i];
             }
             
-            if (command[0] == 1) val1 = command[1];
-            else if (command[0] == 2) val2 = command[1];
-            else printf("Command param invalid\n");
+            if (command[0] < 3 && command[0] > 0) {
+                printf("writing %d to %d\n", command[1], command[0]);
+                vals[command[0] - 1] = command[1];   
+            } else {
+                printf("Command param invalid\n");   
+            }
+           
             Cy_BLE_GATTS_WriteRsp(writeReqParameter->connHandle);
             break;
         case CY_BLE_EVT_GATTS_READ_CHAR_VAL_ACCESS_REQ:
-            printf("Read req");
+            printf("Read req: ");
             cy_stc_ble_gatts_char_val_read_req_t *readReqParam = (cy_stc_ble_gatts_char_val_read_req_t *) eventParameter;
             
             switch (readReqParam->attrHandle)
             {
                 case (CY_BLE_NSTIM_READFIRST_CHAR_HANDLE):
-                    CY_BLE_GATT_DB_ATTR_SET_GEN_VALUE(readReqParam->attrHandle, &val1, 4);
+                    printf("reading ReadFirst\n");
+                    CY_BLE_GATT_DB_ATTR_SET_GEN_VALUE(readReqParam->attrHandle, &vals[0], 4);
                     break;
                 case (CY_BLE_NSTIM_READSECOND_CHAR_HANDLE):
-                    CY_BLE_GATT_DB_ATTR_SET_GEN_VALUE(readReqParam->attrHandle, &val2, 4);
+                    printf("reading ReadSecond\n");
+                    CY_BLE_GATT_DB_ATTR_SET_GEN_VALUE(readReqParam->attrHandle, &vals[1], 4);
                     break;
                 default:
                     printf("Broken\n");
             }
+            break;
         default:
-            printf("Not found\n");
+            printf("BLE request not found\n");
+            
             break;
     }
 }
